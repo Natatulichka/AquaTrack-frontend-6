24d7d32fc9b9cdb1a0641f6648d4cd2d01c813ca
Natalia Huliaieva <155992841+Natatulichka@users.noreply.github.com>
2024-12-05 22:03:36 +0100

    save

--- src/components/DailyInfo/ChooseDate.jsx
+++ src/components/DailyInfo/ChooseDate.jsx
@@ -4,10 +4,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectChoosenDay } from '../../redux/common/selectors';
 import { useEffect } from 'react';
 import { setSelectedDay } from '../../redux/common/operations';
+import { useParams } from 'react-router';
 
 const ChooseDate = () => {
   const { t } = useTranslation();
+  const { date } = useParams();
+  const { isToday, day, month } = getDateMonthString(parseDateTime(date));
   const dispatch = useDispatch();
+  //   const dateString = isToday
+  //   ? t('chooseDate.today')
+  //   : `${day} ${t(month + 'Day')}`;
+  // return <h3 className={css.selectedDate}>{dateString}</h3>;
   const selectedDay = useSelector(selectChoosenDay);
 
   useEffect(() => {
